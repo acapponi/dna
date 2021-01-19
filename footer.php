dc59cb64c79c1409580f54e56800cb67ce224700
Agostina Capponi <capponi.agostina@gmail.com>
2021-01-19 09:02:00 -0300

    Bootstrap 4.4.1

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </main>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="assets/timple-bs/js/bootstrap.min.js"></script>
+<script src="assets/bootstrap-4.4.1/js/bootstrap.min.js"></script>
 <script src="assets/slick-1.8.1/slick.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
