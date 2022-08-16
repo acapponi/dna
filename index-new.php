25a81a4e791fb3cfa26a448bc07f8015bf8e0536
acapponi <capponi.agostina@gmail.com>
2022-08-16 14:31:45 -0300

    new index

---
+++ index-new.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>{Nombre del sitio}</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+    <style>
+        body{
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
+
+</head>
+<body>
+
+	<div style="background-color:#30ffc4;" class="py-5">
+		<div class="container text-center">
+            <div class="row justify-content-center">
+                <div class="col-md-9">
+                    <img src="temp/timplecrew-logo.svg" width="350">
+                    <h1 class="mt-3">Front-end development</h1>
+                    <hr class="my-4">
+                    <h2>Site: {Nombre del sitio}</h2>
+                </div>
+            </div>
+		</div>
+	</div>
+
+	<div class="container py-4">
+		<div class="row justify-content-center">
+			<div class="col-md-9">
+				<div class="list-group list-group-flush ">
+					<a href="home.php" class="list-group-item list-group-item-action">Home</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
